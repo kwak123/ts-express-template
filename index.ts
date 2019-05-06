ea7b3cf3a8421c4f5ac0531db132c5136ae63cd5
kwak123 <kwak123@gmail.com>
2019-05-06 08:22:43 -0400

    first version

---
+++ index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+
+const port = "3001";
+
+// tslint:disable-next-line:no-console
+app.listen(port, () => console.log(`Now listening to ${port}`));
