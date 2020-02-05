11429f4855dd9b1d98175a303aff2bd0277c57c6
kwak123 <kwak123@gmail.com>
2020-02-05 10:59:35 -0500

    update vulnerabilities

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-const port = "3001";
+const port = '3001';
 
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`Now listening to ${port}`));
